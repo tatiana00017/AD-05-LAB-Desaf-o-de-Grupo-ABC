4bc9687a9e856e97d008bfa7ee6ccf05d9c62989
Esteban <estebancanoc@gmail.com>
2024-09-06 13:57:18 -0500

    Delete innecesary functions to be hanndled by styles.css

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ function arrClicks() {
   // Obtener todos los elementos h5
   const h5Elements = document.getElementsByTagName("h5");
 
-  // Obtener todos los elementos h3 con el nombre de la clase personalizado.
-  const subtitleElements = document.getElementsByClassName("subtitle");
-
   // Asignar el evento onclick a cada elemento de la clase "display-1"
   for (let i = 0; i < displayElements.length; i++) {
     displayElements[i].onclick = function () {
@@ -20,13 +17,6 @@ function arrClicks() {
   for (let i = 0; i < h5Elements.length; i++) {
     h5Elements[i].onclick = function () {
       applyRandomColorToElement(h5Elements[i]);
-    };
-  }
-
-  // Asignar el evento onclick a cada elemento de la clase "Subtitle"
-  for (let i = 0; i < subtitleElements.length; i++) {
-    subtitleElements[i].onclick = function () {
-      subtitleElements[i].style.color = "blue";
     };
   }
 }
@@ -41,11 +31,6 @@ function applyRandomColorToElement(element) {
 
   // Aplicar el color al estilo del elemento
   element.style.color = randomColor;
-}
-
-// Obtener 1 - h3 para convertirlo a "red"
-function subtitleToRed() {
-  document.getElementById("subtitleRed").style.color = "red";
 }
 
 // Llamar a la funcion arrClicks cuando la página haya cargado
